03d31e89547c3849ebc5818dcdfee3e7df53961f
Kevin Rosario <29806319+KevinRosario01@users.noreply.github.com>
2024-06-13 02:41:54 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,54 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const cardValue = document.querySelector(".card-value");
+  cardValue.innerHTML = pickCard();
+
+  const cardSuits = document.querySelectorAll(".card-suit");
+  let suit = pickSuit();
+  cardSuits.forEach(cardSuit => {
+    cardSuit.innerHTML = suit;
+    if (suit === "♦" || suit === "♥") {
+      cardSuit.style.color = "red";
+    }
+  });
+
+  console.log("Suit: " + pickSuit());
+  console.log("Card: " + pickCard());
 };
+
+function pickSuit() {
+  let suit = Math.floor(Math.random() * 4);
+
+  const numToSuit = {
+    "0": "♣",
+    "1": "♠",
+    "2": "♦",
+    "3": "♥"
+  };
+  // ♠ ♣ ♥ ♦
+  return numToSuit[suit];
+}
+
+function pickCard() {
+  let card = Math.floor(Math.random() * 12);
+
+  const numToCard = {
+    "0": "2",
+    "1": "3",
+    "2": "4",
+    "3": "5",
+    "4": "6",
+    "5": "7",
+    "6": "8",
+    "7": "9",
+    "8": "10",
+    "9": "J",
+    "10": "Q",
+    "11": "K",
+    "12": "A"
+  };
+
+  return numToCard[card];
+}
